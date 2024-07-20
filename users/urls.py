e9f9afb928939e892203eedd5e880f5a1174b251
galaxy4276 <deveungi@gmail.com>
2024-07-20 23:53:45 +0900

    feat: 카카오 로그인 URL 연결

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,7 @@ from .api import *
 
 
 urlpatterns = [
-    path("auth/signup/", SignupAPI.as_view()),
-    path("auth/kakao/url/", KakaoRedirectAPI.as_view()),
+    path("auth/signup", SignupAPI.as_view()),
+    path("auth/kakao/url", KakaoRedirectAPI.as_view()),
+    path("auth/kakao/login", KakaoLoginAPI.as_view()),
 ]

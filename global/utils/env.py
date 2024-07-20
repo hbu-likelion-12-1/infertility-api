37ff5fb1c9b178cfc96a92973d40d0dd5dc8e1c6
galaxy4276 <deveungi@gmail.com>
2024-07-20 09:36:00 +0900

    feat: 환경변수를 가져오는 유틸 객체 작성

---
+++ global/utils/env.py
@@ -0,0 +1,22 @@
+import os
+
+gpt_key: str = os.getenv("GPT_KEY")
+kakao_rest_api_key: str = os.getenv("KAKAO_REST_KEY")
+
+if gpt_key is None:
+    pass
+
+
+class AppEnvironment:
+    @staticmethod
+    def gpt_key():
+        if not gpt_key:
+            raise EnvironmentError("GPT_KEY environment variable not set")
+        return gpt_key
+
+    @staticmethod
+    def kakao_rest_api():
+        if not kakao_rest_api_key:
+            raise EnvironmentError(
+                "KAKAO_REST_KEY environment variable not set")
+        return kakao_rest_api_key

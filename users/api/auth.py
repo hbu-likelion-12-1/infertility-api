ee529e2eccc6c520c0f89bb6485ce62a2dc52751
galaxy4276 <deveungi@gmail.com>
2024-07-21 00:09:09 +0900

    feat: 카카오 로그인 APIView

--- users/api/auth.py
+++ users/api/auth.py
@@ -1,8 +1,10 @@
 from rest_framework.views import APIView, Request, Response
 from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 from ..serializers import UserSerializer
 from app.utils import AppEnvironment
 from rest_framework.permissions import AllowAny
+from users.kakao import KakaoProvider
 
 
 # 회원가입 API
@@ -25,3 +27,25 @@ class KakaoRedirectAPI(APIView):
     @swagger_auto_schema(operation_summary="카카오 인증 URL 리다이렉트 API")
     def get(self, req):
         return Response(data=AppEnvironment.kakao_auth_url, status=302)
+
+
+class KakaoLoginAPI(APIView):
+    permission_classes = [AllowAny]
+
+    @swagger_auto_schema(
+        operation_summary="카카오 로그인 API",
+        manual_parameters=[
+            openapi.Parameter(
+                "code",
+                openapi.IN_QUERY,
+                description="카카오 인증코드",
+                type=openapi.TYPE_STRING,
+            )
+        ],
+    )
+    def get(self, req: Request):
+        auth_code = req.GET.get("code")
+        kakao = KakaoProvider(auth_code=auth_code)
+        kakao.get_token()
+        kakao_user_id = kakao.login()
+        return Response(status=200)

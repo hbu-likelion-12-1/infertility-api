7b836fa74f2e2afb8e842f1b2491e3c81be420a5
galaxy4276 <deveungi@gmail.com>
2024-07-22 03:45:50 +0900

    feat: 매치 모델 설계 변경

--- question/models.py
+++ question/models.py
@@ -6,13 +6,24 @@ from app.enum import Emotion
 
 
 class Question(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     content = models.CharField(max_length=500, db_comment="질문 내용")
-    female_audio_url = models.URLField(db_comment="아내 음성 데이터 URL")
-    male_audio_url = models.URLField(db_comment="남편 음성 데이터 URL")
+    female_audio_url = models.URLField(db_comment="아내 음성 데이터 URL", null=True)
+    male_audio_url = models.URLField(db_comment="남편 음성 데이터 URL", null=True)
     match = models.ForeignKey(
         Match, related_name="questions", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, db_comment="매칭 체결일")
+
+    @staticmethod
+    def create(content: str, match: Match):
+        question = Question(
+            content=content,
+            match=match,
+            female_audio_url=None,
+            male_audio_url=None
+        )
+        question.save()
+        return question
 
 
 class QuestionAnswer(models.Model):
@@ -25,3 +36,4 @@ class QuestionAnswer(models.Model):
     question = models.ForeignKey(
         Question, related_name="question_answers", on_delete=models.CASCADE
     )
+

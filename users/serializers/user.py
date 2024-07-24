530c05561a09b4c72adb6752d0f8673f85fd8a3e
galaxy4276 <deveungi@gmail.com>
2024-07-24 15:35:30 +0900

    feat: 사용자 질문 id 포함 직렬화 객체 작성

--- users/serializers/user.py
+++ users/serializers/user.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from users.models import User
+from question.handler import MindHandler
 
 
 class UserDetails:
@@ -18,3 +19,23 @@ class UserDetails:
     @staticmethod
     def exists_kakao(kakao_id: str):
         return User.objects.filter(kakao_id=kakao_id).exists()
+
+    class UserWithQuestionId(serializers.ModelSerializer):
+        mind_id = serializers.SerializerMethodField()
+
+        class Meta:
+            model = User
+            exclude = [
+                "last_login",
+                "is_superuser",
+                "kakao_id",
+                "groups",
+                "user_permissions",
+            ]
+
+        def get_mind_id(self, obj: User):
+            mind = MindHandler.first_by_user(user=obj)
+            if not mind:
+                self.mind_id = None
+                return
+            self.mind_id = mind.id

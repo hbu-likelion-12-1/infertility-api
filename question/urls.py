98d68df2fc96f0c4fe0990bfb1393240e2d09bec
galaxy4276 <deveungi@gmail.com>
2024-07-24 14:44:59 +0900

    fix: url 모두 끝에 / 추가

--- question/urls.py
+++ question/urls.py
@@ -3,6 +3,6 @@ from .api import *
 
 
 urlpatterns = [
-    path("<str:match_id>", QuestionAPI.as_view()),
-    path("mind/<int:question_id>", MindAnswerAPI.as_view()),
+    path("<str:match_id>/", QuestionAPI.as_view()),
+    path("mind/<int:question_id>/", MindAnswerAPI.as_view()),
 ]
--- users/urls.py
+++ users/urls.py
@@ -3,9 +3,9 @@ from .api import *
 
 
 urlpatterns = [
-    path("auth/signup", SignupAPI.as_view()),
-    path("auth/kakao/url", KakaoRedirectAPI.as_view()),
-    path("auth/login", KakaoLoginAPI.as_view()),
+    path("auth/signup/", SignupAPI.as_view()),
+    path("auth/kakao/url/", KakaoRedirectAPI.as_view()),
+    path("auth/login/", KakaoLoginAPI.as_view()),
     path("", UserCrudAPI.as_view()),
-    path("test", TestUserAPI.as_view()),
+    path("test/", TestUserAPI.as_view()),
 ]

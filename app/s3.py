c671520ae10dcdf581c0427166384323a7570402
galaxy4276 <deveungi@gmail.com>
2024-07-25 16:31:47 +0900

    feat: s3 파일 업로드 객체 작성

---
+++ app/s3.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from botocore.config import Config
+from app.utils import AppEnvironment
+import boto3
+
+s3_details = AppEnvironment.s3_details()
+config = Config(signature_version="v4")
+s3 = boto3.client(
+    "s3",
+    config=config,
+    aws_access_key_id=s3_details["access_key"],
+    aws_secret_access_key=s3_details["secret_key"],
+    region_name=s3_details["region"],
+)
+
+
+class S3FileUploadSerializer(serializers.Serializer):
+    file = serializers.FileField()
+
+    def create(self, validated_data):
+        file = validated_data.get("file")
+        filename = file.name
+
+        s3.upload_fileobj(file, s3_details["bucket_name"], filename)
+        return {"file_url": f'{s3_details["cloudfront_url"]}/{filename}'}

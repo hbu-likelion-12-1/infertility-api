b80ec7a4919161adfc2e3840f2025bf255eecb5e
galaxy4276 <deveungi@gmail.com>
2024-07-25 23:49:39 +0900

    rollback: 정적 파일 관련 설정 복구

--- app/settings.py
+++ app/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -137,7 +136,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 

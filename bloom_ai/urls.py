678641250c38d29c121fd6efcacca71cb7b57964
lina071350 <lina071350@gmail.com>
2024-08-02 22:44:18 +0900

    feat: 추천 시스템

--- bloom_ai/urls.py
+++ bloom_ai/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .api import *
+from .views import recommend_view
 
 urlpatterns = [
     path("", BloomAPI.as_view()),
+    path('recommend/', recommend_view, name='recommend')
 ]

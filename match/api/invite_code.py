3b8f9b93c8f54e3ddfbb44c765eac457998aa430
galaxy4276 <deveungi@gmail.com>
2024-08-03 02:22:22 +0900

    fix: 초대 코드 가져오기 API 오류 수정

--- match/api/invite_code.py
+++ match/api/invite_code.py
@@ -10,7 +10,7 @@ class InviteCodeAPI(APIView):
             InviteCode.objects.filter(
                 creator=req.user).order_by("-created_at").first()
         )
-        return Response(data={"invite_code": invite_code}, status=200)
+        return Response(data=invite_code.code, status=200)
 
     @swagger_auto_schema(operation_summary="초대 코드 발급 API")
     def post(self, req: Request):

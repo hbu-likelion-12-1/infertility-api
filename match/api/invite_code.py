1e4962cd10e5e45451c9e802c2819dea19398c8c
galaxy4276 <deveungi@gmail.com>
2024-08-02 22:34:55 +0900

    feat: 초대 코드 조회 API

--- match/api/invite_code.py
+++ match/api/invite_code.py
@@ -4,6 +4,14 @@ from drf_yasg.utils import swagger_auto_schema
 
 
 class InviteCodeAPI(APIView):
+    @swagger_auto_schema(operation_summary="초대 코드 조회 API")
+    def get(self, req: Request):
+        invite_code = (
+            InviteCode.objects.filter(
+                creator=req.user).order_by("-created_at").first()
+        )
+        return Response(data={"invite_code": invite_code}, status=200)
+
     @swagger_auto_schema(operation_summary="초대 코드 발급 API")
     def post(self, req: Request):
         invite_code: InviteCode = InviteCode(creator=req.user).save()

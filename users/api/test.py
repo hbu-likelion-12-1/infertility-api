ce5cdfb5dad8f6e294fbd9cb1d7bbc7b09dcc52e
galaxy4276 <deveungi@gmail.com>
2024-08-03 10:09:43 +0900

    feat: 시연용 데이터 삭제 API 작성

--- users/api/test.py
+++ users/api/test.py
@@ -3,6 +3,10 @@ from users.api.login import LoginService
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 from rest_framework.permissions import AllowAny
+from users.models import User
+from question.models import Question
+from match.models import Match, InviteCode
+from match.handler import MatchHandler
 
 
 class TestUserAPI(APIView):
@@ -24,6 +28,18 @@ class TestUserAPI(APIView):
         kakao_id = self.get_kakao_id(type)
         user_details = LoginService.login(kakao_id)
         return Response(data=user_details, status=200)
+
+    # 시연 종료로 모든 테스트 계정 관련 데이터를 삭제합니다.
+    def post(self, req: Request):
+        user: User = req.user
+        match = MatchHandler.find_by_user(user)
+        invite_codes = InviteCode.objects.filter(creator=user)
+        questions = Question.objects.filter(match=match)
+
+        invite_codes.delete()
+        questions.delete()
+        match.delete()
+        return Response(status=200)
 
     def get_kakao_id(self, type: str):
         if type == "wife":
